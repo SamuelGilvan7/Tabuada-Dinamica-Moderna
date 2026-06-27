ff21f42fa245ef26a3df29778ec8db55e2f4d9cf
SamuelGilvan7 <Sankiragilvan@gmail.com>
2026-06-27 09:47:49 -0300

    Primeiro commit da calculadora

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('#form-tabuada');
+
+form.addEventListener('submit', function(event){
+
+    event.preventDefault();
+
+    let resposta = event.target.querySelector('#seltab');
+    let numm = event.target.querySelector('#txtn');
+    let num = setValor(numm);
+
+    resposta.innerHTML = ''; 
+
+    calularTabuada(num, resposta);
+
+});
+
+function setValor(num){
+    return Number(num.value);
+}
+
+function calularTabuada(num, resposta){ 
+    
+    for(let i = 0; i <= 10; i++) { 
+        let item = document.createElement('option'); 
+        item.text = `${num} x ${i} = ${num * i}`; 
+        item.value = `val${i}`; 
+        resposta.appendChild(item); 
+    } 
+}
