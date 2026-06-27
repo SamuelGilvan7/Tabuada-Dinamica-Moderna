b9e1020f623bd044cda00e34d599f134400b23c5
SamuelGilvan7 <Sankiragilvan@gmail.com>
2026-06-27 10:13:26 -0300

    Acabei de reforça a segurança

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ form.addEventListener('submit', function(event){
 
     let resposta = event.target.querySelector('#seltab');
     let numm = event.target.querySelector('#txtn');
+
+    if (numm.value.length === 0) {
+        alert("Por favor, digite um número válido!");
+        return; 
+    }
+
     let num = setValor(numm);
 
     resposta.innerHTML = ''; 
